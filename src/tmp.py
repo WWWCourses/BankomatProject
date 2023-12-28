ed42ab7e9fdd8262477ca83df16d5bc61268cf72
WWWCourses <ivawebcourses@gmail.com>
2023-12-28 16:57:26 +0200

    clean tmp

--- src/tmp.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class MenuOptions(Enum):
-    WITHDRAW_MONEY = (1, "Withdraw Money")
-    DEPOSIT_MONEY = (2, "Deposit Money")
-    SHOW_ACCOUNT_DETAILS = (3, "Show Account Details")
-
-for opt in MenuOptions:
-    print(opt.value)
